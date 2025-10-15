007dbda03a544fec4a47e98ad27144ad4f99e678
Damim-03 <imadeddinekir95@gmail.com>
2025-10-15 08:41:49 +0100

    First commit

---
+++ calculPar/TP03.py
@@ -0,0 +1,14 @@
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+    data = {"a": 7, "b": 3.14}
+    comm.send(data, dest=1, tag=11)
+    print(f"Process {rank} sent data: {data}")
+
+elif rank == 1:
+    data = comm.recv(source=0, tag=11)
+    print(f"Process {rank} received data: {data}")
